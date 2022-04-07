87c47c2efe08a5c6e2344e94acc6ce833b172b3d
divyanshu0201 <71318423+divyanshu0201@users.noreply.github.com>
2022-04-07 15:17:37 +0530

    Create 1.py

---
+++ lab1/1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+data=pd.read_csv("data.csv")
+print(data)
+
+
+//output//
+ sky air temp humidity    wind water forecast enjoy sport
+0  sunny     warm   normal  strong  warm     same         yes
+1  sunny     warm     high  strong  warm     same         yes
+2  rainy     cold     high  strong  warm   change          no
+3  sunny     warm     high  strong  cool   change         yes
